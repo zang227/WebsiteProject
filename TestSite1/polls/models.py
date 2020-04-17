99884dc6d6c54b4b975d29aa36db20e205eee485
tsteudlein <46979581+tsteudlein@users.noreply.github.com>
2020-04-17 11:01:56 -0500

    Database Correction and Simplication
    I corrected the database relationships and simpified the schema in general to allow for easier time inserting test data.

--- TestSite1/polls/models.py
+++ TestSite1/polls/models.py
@@ -4,14 +4,12 @@ class Company(models.Model):
     company_name = models.CharField(max_length=100)
     company_address = models.CharField(max_length=100)
     company_description = models.CharField(max_length=200)
-    company_employee = models.ForeignKey('Employee', null=True, on_delete=models.SET_NULL)
-    company_job = models.ForeignKey('Job', null=True, on_delete=models.SET_NULL)
+    company_association = models.ForeignKey('Association', null=True, on_delete=models.SET_NULL)
     
 class Association(models.Model):
     association_name = models.CharField(max_length=100)
     association_address = models.CharField(max_length=100)
     association_description = models.CharField(max_length=200)
-    association_company = models.ForeignKey('Company', null=True, on_delete=models.SET_NULL)
 
 class Employee(models.Model):
     employee_name = models.CharField(max_length=100)
@@ -26,15 +24,10 @@ class Job(models.Model):
     job_date = models.DateTimeField('date posted')
     job_status = models.CharField(max_length=100)
     job_company = models.ForeignKey('Company', null=True, on_delete=models.SET_NULL)
-    job_applicant = models.ForeignKey('Applicant', null=True, on_delete=models.SET_NULL)
-    job_search_committee = models.ForeignKey('Search_Committee', null=True, on_delete=models.SET_NULL)
 
 class Applicant(models.Model):
     applicant_name = models.CharField(max_length=100)
     applicant_last_name = models.CharField(max_length=100)
     applicant_resume = models.CharField(max_length=500)
     application_status = models.CharField(max_length=100)
-
-class Search_Committee(models.Model):
-    SC_employee = models.ForeignKey('Employee', null=True, on_delete=models.SET_NULL)
-    SC_job = models.ForeignKey('Job', null=True, on_delete=models.SET_NULL)
+    applicant_job = models.ForeignKey('Job', null=True, on_delete=models.SET_NULL)
