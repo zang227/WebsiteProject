e51efe14304a241e3adedb1328db866a651cd7df
tsteudlein <46979581+tsteudlein@users.noreply.github.com>
2020-04-18 12:26:30 -0500

    INSERT TEST DATA and db updates
    Inserting all test data from TA's email. Also a few db updates as I see them.

--- TestSite1/polls/models.py
+++ TestSite1/polls/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Company(models.Model):
     company_name = models.CharField(max_length=100)
     company_address = models.CharField(max_length=100)
-    company_description = models.CharField(max_length=200)
+    company_description = models.CharField(max_length=1000)
     company_association = models.ForeignKey('Association', null=True, on_delete=models.SET_NULL)
     
     def __str__(self):
@@ -12,7 +12,7 @@ class Company(models.Model):
 class Association(models.Model):
     association_name = models.CharField(max_length=100)
     association_address = models.CharField(max_length=100)
-    association_description = models.CharField(max_length=200)
+    association_description = models.CharField(max_length=1000)
     
     def __str__(self):
         return self.association_name
@@ -21,8 +21,7 @@ class Employee(models.Model):
     employee_name = models.CharField(max_length=100)
     employee_last_name = models.CharField(max_length=100)
     employee_email = models.CharField(max_length=100)
-    employee_address = models.CharField(max_length=100)
-    employee_resume = models.CharField(max_length=500)
+    is_employer = models.CharField(max_length=100)
     employee_company = models.ForeignKey('Company', null=True, on_delete=models.SET_NULL)
     
     def __str__(self):
@@ -30,6 +29,7 @@ class Employee(models.Model):
 
 class Job(models.Model):
     job_title = models.CharField(max_length=100)
+    job_qualifications = models.CharField(max_length=1000)
     job_date = models.DateTimeField('date posted')
     job_status = models.CharField(max_length=100)
     job_company = models.ForeignKey('Company', null=True, on_delete=models.SET_NULL)
@@ -41,7 +41,9 @@ class Applicant(models.Model):
     applicant_name = models.CharField(max_length=100)
     applicant_last_name = models.CharField(max_length=100)
     applicant_email = models.CharField(max_length=100)
-    applicant_resume = models.CharField(max_length=500)
+    applicant_password = models.CharField(max_length=100)
+    applicant_address = models.CharField(max_length=100)
+    applicant_resume = models.CharField(max_length=1000)
     application_status = models.CharField(max_length=100)
     applicant_job = models.ForeignKey('Job', null=True, on_delete=models.SET_NULL)
     
