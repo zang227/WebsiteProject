83fd4bbd99a19ffac2d473016b0fa6986f3fc7d3
spottl1 <spottl1@lsu.edu>
2020-04-18 23:11:23 -0500

    update

--- TestSite1/polls/views.py
+++ TestSite1/polls/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import get_object_or_404, render, redirect
-from .models import Applicant, Employee, Company
-from .forms import SignUpForm, SignUpForm2
+from django.shortcuts import get_object_or_404, render
+from .models import Applicant, Employee
 
 
 
@@ -8,49 +7,14 @@ def login(request):
     return render(request, 'polls/login.html')
 
 def signup(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            # email = form.cleaned_data['applicant_email']
-            # password = form.cleaned_data['applicant_password']
-            # firstName = form.cleaned_data['applicant_name']
-            # lastName = form.cleaned_data['applicant_last_name']
-            q = Applicant.objects.get(applicant_email = email)
-        return redirect('/home/'+str(q.id))
-    else:
-        form = SignUpForm()
-        return render(request, 'polls/signup.html', {'form':form})
+    return render(request, 'polls/signup.html')
 
 def signup2(request):
-    if request.method == 'POST':
-        form1 = SignUpForm(request.POST)
-        form2 = SignUpForm2(request.POST)
-        if form1.is_valid():
-            form1.save()
-            if form2.is_valid():
-                email = form1.cleaned_data['applicant_email']
-                password = form1.cleaned_data['applicant_password']
-                firstName = form1.cleaned_data['applicant_name']
-                lastName = form1.cleaned_data['applicant_last_name']
-                company = form2.cleaned_data['employee_company']
-                is_employer = form2.cleaned_data['is_employer']
-                r = Company.objects.get(company_name = company)
-                t = Employee(employee_name = firstName, employee_last_name=lastName, employee_email=email, employee_company=r, is_employer=is_employer)
-                t.save()
-                q = Applicant.objects.get(applicant_email = email)
-            return redirect('/home/'+str(q.id))
-        else:
-            form1 = SignUpForm()
-            form2 = SignUpForm2()
-            return render(request, 'polls/signup2.html', {'form1':form1, 'form2':form2})
-    else:
-        form1 = SignUpForm()
-        form2 = SignUpForm2()
-        return render(request, 'polls/signup2.html', {'form1':form1, 'form2':form2})
+    return render(request, 'polls/signup2.html')
 
-def search(request):
-    return render(request, 'polls/search.html')
+def search(request, applicant_id):
+    applicant = get_object_or_404(Applicant, pk=applicant_id)
+    return render(request, 'polls/search.html', {'applicant': applicant})
 
 def home(request, applicant_id):
     applicant = get_object_or_404(Applicant, pk=applicant_id)
@@ -61,10 +25,18 @@ def report(request, applicant_id):
     return render(request, 'polls/report.html', {'applicant': applicant})
 
 def profile(request, applicant_id):
-    applicant = get_object_or_404(Applicant, pk=applicant_id)
-    try:
-        employee = Employee.objects.get(employee_email = applicant.applicant_email)
-        return render(request, 'polls/profile.html', {'applicant': applicant, 'employee':employee})
-    except Employee.DoesNotExist:
-        return render(request, 'polls/profile.html', {'applicant': applicant})
+    realid = decryptor(applicant_id)
+    applicant = get_object_or_404(Applicant, pk=realid)
+    employee = Employee.objects.get(employee_email = applicant.applicant_email)
+    return render(request, 'polls/profile.html', {'applicant': applicant, 'employee':employee})
     
+def decryptor (applicant_id):
+    id = ((((applicant_id - 3) / 2) + 6) / 65) / 5
+    return id
+
+
+
+
+
+def encryptor(id):
+    newid = ((((id * 5) * 65) - 6) * 2) + 3
