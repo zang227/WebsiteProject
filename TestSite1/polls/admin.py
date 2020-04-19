3eb9191f165cb785090820ecec1e4329e5fa2672
tsteudlein <46979581+tsteudlein@users.noreply.github.com>
2020-04-19 12:55:13 -0500

    Merge branch 'master' of https://github.com/zang227/WebsiteProject

--- TestSite1/polls/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Company, Association, Employee, Job, Applicant
-
-admin.site.register(Company)
-admin.site.register(Association)
-admin.site.register(Employee)
-admin.site.register(Job)
-admin.site.register(Applicant)
