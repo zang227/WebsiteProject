1168e73a3e93cc4d451b291dfc7691ccce5cd834
spottl1 <spottl1@lsu.edu>
2020-04-20 14:36:44 -0500

    commit

--- TestSite1/polls/views.py
+++ TestSite1/polls/views.py
@@ -188,13 +188,14 @@ def report(request, applicant_id):
     company_id = Company.objects.get(employee__employee_email=id)
     job_id = Job.objects.filter(job_company=company_id)
     applicant_loop = Applicant.objects.filter(applicant_job__in=job_id)
-    app_Count = applicant_loop.values('applicant_job').annotate(appCount=Count('applicant_job')).order_by('-appCount')
-    print(app_Count)
 
     applied = Applicant.objects.filter(applicant_job__isnull=False)
+    appliedJobs = applied.values('applicant_job')
     company_all = Company.objects.filter(employee__employee_email__in=list(applied)).values('company_name').annotate(
         ccount=Count('company_name')).order_by('-ccount')[:4]
 
+    app_Count = Job.objects.filter(pk__in=appliedJobs).values('job_title').annotate(appCount=Count('job_title')).order_by('-appCount')
+    print(app_Count)
     job_most = applied.values('applicant_job').annotate(jcount=Count('applicant_job')).order_by('-jcount')[0]
     top = Applicant.objects.filter(application_status__exact='Hired').count()
     bottom = Applicant.objects.filter(applicant_job__isnull=False).count()
